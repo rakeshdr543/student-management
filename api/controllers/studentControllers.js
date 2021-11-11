dc77bb27a10d0136b66707c608577418428240e5
Rakesh D R <rakeshdr543@gmail.com>
2021-11-11 08:05:56 +0530

    Added finished backend api

--- api/controllers/studentControllers.js
+++ api/controllers/studentControllers.js
@@ -1,12 +1,13 @@
 const Student = require('../models/Student');
 const { StatusCodes } = require('http-status-codes');
+const CustomError=require('../errors/custom-api')
+
 const cloudinary=require('cloudinary').v2
 const fs=require('fs')
 
 // Create Student   =>    POST /api/v1/students
 const createStudent = async (req, res) => {
-  req.body.user = req.user.userId;
-  const Student = await Student.create(req.body);
+  const student = await Student.create(req.body);
   res.status(StatusCodes.CREATED).json({ student });
 };
 
@@ -14,9 +15,71 @@ const createStudent = async (req, res) => {
 // Get All Students   =>    GET /api/v1/students
 
 const getAllStudents = async (req, res) => {
-  const Students = await Student.find({});
-  res.status(StatusCodes.OK).json({ Students, count: Students.length });
+  const {sort}=req.query
+
+  let result = Student.find({})
+
+  if(sort){
+    const sortList=sort.split(',').join(' ')
+    result=result.sort(sortList)
+  }else{
+    result=result.sort('fName')
+  }
+
+  const page=Number(req.query.page) || 1
+  const limit=Number(req.query.limit) || 10
+  const skip=(page-1)*limit
+
+  result=result.skip(skip).limit(limit)
+
+  const students=await result
+
+  res.status(StatusCodes.OK).json({ students, count: students.length });
 };
+
+// Get Single Student   =>    GET /api/v1/students/:id
+
+const getSingleStudent=async(req,res)=>{
+  const {id:studentId}=req.params
+
+  const student=await Student.findOne({_id:studentId})
+
+  if(!student){
+    throw new CustomError.NotFoundError(`No student with id : ${studentId}`)
+  }
+  res.status(StatusCodes.OK).json({ student });
+}
+
+// Update Student   =>    PATCH /api/v1/students/:id
+
+const updateStudent=async(req,res)=>{
+  const {id:studentId}=req.params
+
+  const student=await Student.findOneAndUpdate({_id:studentId},req.body,{
+    new:true,
+    runValidators:true
+  })
+
+  if(!student){
+    throw new CustomError.NotFoundError(`No student with id : ${studentId}`)
+  }
+  res.status(StatusCodes.OK).json({ student });
+}
+
+// Delete Student   =>    DELETE /api/v1/students/:id
+
+const deleteStudent=async(req,res)=>{
+  const {id:studentId}=req.params
+
+  const student=await Student.findOne({_id:studentId})
+
+  if(!student){
+    throw new CustomError.NotFoundError(`No student with id : ${studentId}`)
+  }
+
+  student.remave()
+  res.status(StatusCodes.OK).json({ msg:'success! student removed'});
+}
 
 const uploadImage = async (req, res) => {
   const result = await cloudinary.uploader.upload(
@@ -34,4 +97,7 @@ return res.status(StatusCodes.OK).json({image:{src:result.secure_url}})
     createStudent,
     getAllStudents,
     uploadImage,
+    updateStudent,
+    deleteStudent,
+    getSingleStudent
   };
--- api/routes/studentRoutes.js
+++ api/routes/studentRoutes.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const router = express.Router();
 
-const {createStudent,getAllStudents,uploadImage}=require('../controllers/studentControllers')
+const {createStudent,getAllStudents,uploadImage, getSingleStudent, updateStudent, deleteStudent}=require('../controllers/studentControllers')
 
 router.route('/')
     .post(createStudent)
     .get(getAllStudents)
+
+router.route('/:id')
+    .get(getSingleStudent)
+    .patch(updateStudent)
+    .delete(deleteStudent)
 
 router.route('/uploadImage')
     .post(uploadImage)
